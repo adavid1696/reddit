afe31cfbd600d24e8f1187afb5a09bd5e23b89ca
Anthony <adavid16967@gmail.com>
2022-05-31 14:46:33 -0400

    boilerplate added

---
+++ server/server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const path = require('path');
+const PORT = 3000;
+const app = express();
+const cors = require('cors');
+
+ 
+// module exports = chunk of codes are modules
+app.use(express.json());
+// lets you view information
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '../dist')));
+app.use(cors());
+
+// const userRouter = require('./routers/userRouter.js');
+// const itemRouter = require('./routers/itemRoutes.js')
+
+// app.use('/users', userRouter)
+// app.use('/items', itemRouter)
+
+
+app.use('*', (req, res) => {
+	return res
+		.status(404)
+		.json({ err: 'endpoint requested is not found' });
+});
+
+app.use((err, req, res, next) => {
+	const defaultErr = {
+		log: `Express error handler caught unknown middleware error ${err}`,
+		status: 500,
+		message: {
+			err: 'An error occurred. Please contact the Astro team.',
+		},
+	};
+
+  const errorObj = Object.assign({}, defaultErr, err);
+
+	return res.status(errorObj.status).json(errorObj.message);
+  
+});
+
+app.listen(PORT, () => {
+	console.log(`Server listening on port: ${PORT}...`)
+})
+
+// for testing
+module.exports = app
